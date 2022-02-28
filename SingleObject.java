006e66e2a0304ecfda62e7d9967c56c2e0a03d56
Rohini-DevOps123 <100296806+Rohini-DevOps123@users.noreply.github.com>
2022-02-28 13:47:19 +0530

    Create SingleObject.java

---
+++ SingleObject.java
@@ -0,0 +1,18 @@
+public static SingleObject getInstance()
+{
+return instance;
+}
+ public boolean max(int a, int b)
+ {
+if(a > b)
+ {
+return true;
+}
+ else if (a == b) {
+return false;
+} else
+ {
+return false;
+}
+}
+}
